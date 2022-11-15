8abb245cd538344473455910c05fa9ec77277d5f
Link <h26281660@gmail.com>
2022-11-15 21:47:00 +0800

    separate pdf util function

--- src/utils/base64pdfToCanvas.ts
+++ src/utils/base64pdfToCanvas.ts
@@ -3,6 +3,59 @@ import { pathConverter } from './pathConverter'
 
 pdfjsLib.GlobalWorkerOptions.workerSrc = pathConverter('/lib/pdf.worker.min.js')
 
+export const handlePdf = {
+  buildPdfDoc: async (base64pdf: string) => {
+    const data = atob(base64pdf.replace(/.*base64,/, ''))
+    const pdfDoc = await pdfjsLib.getDocument({ data }).promise
+
+    return pdfDoc
+  },
+  getPage: async (pdfDoc: pdfjsLib.PDFDocumentProxy, page: number) => {
+    const pdfPage = await pdfDoc.getPage(page)
+
+    return pdfPage
+  },
+  getCanvas: (
+    pdfPage: pdfjsLib.PDFPageProxy,
+    config: {
+      containerWidth: number
+      containerHeight: number
+      scale: 'fullWidth' | 'fullHeight' | number
+    }
+  ) => {
+    const { containerWidth, containerHeight, scale } = config
+    const canvas = document.createElement('canvas')
+    let viewport: pdfjsLib.PageViewport
+
+    if (typeof scale === 'number') {
+      viewport = pdfPage.getViewport({ scale })
+
+      canvas.height = viewport.height
+      canvas.width = viewport.width
+    } else {
+      const viewportBase = pdfPage.getViewport({ scale: 1 })
+
+      if (scale === 'fullWidth') {
+        const pdfWidthBase = viewportBase.width
+        const currentScale = containerWidth / pdfWidthBase
+        viewport = pdfPage.getViewport({ scale: currentScale })
+
+        canvas.height = viewport.height
+        canvas.width = viewport.width
+      } else if (scale === 'fullHeight') {
+        const pdfHeightBase = viewportBase.height
+        const currentScale = containerHeight / pdfHeightBase
+        viewport = pdfPage.getViewport({ scale: currentScale })
+
+        canvas.height = viewport.height
+        canvas.width = viewport.width
+      }
+    }
+
+    return canvas
+  }
+}
+
 export const base64pdfToCanvas = async (base64pdf: string) => {
   const data = atob(base64pdf.replace(/.*base64,/, ''))
 
