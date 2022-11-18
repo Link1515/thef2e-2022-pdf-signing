77f1a922223175e6ad85ae35d5292f0f1cc79381
Link <link.lin@udngroup.com>
2022-11-18 17:30:34 +0800

    build sign preview

--- src/components/EditPanel.tsx
+++ src/components/EditPanel.tsx
@@ -1,20 +1,39 @@
-import { useState } from 'react'
 import iconPlus from '../assets/images/icons/plus.png'
+import { useState, useRef, useEffect } from 'react'
+import { useSignStore } from '../store'
+import { getElementContentWidth } from '../utils'
 import ModelSign from './ModelSign'
+import ImageUrlPreview from './ImageUrlPreview'
 
 const EditPanel = () => {
   const [showModel, setModelState] = useState(false)
+  const editPanel = useRef<HTMLDivElement>(null)
+  const [imageUrlPreviewWidth, setImageUrlPreviewWidth] = useState(0)
+  const signStore = useSignStore()
+
+  useEffect(() => {
+    if (!editPanel.current) return
+    setImageUrlPreviewWidth(getElementContentWidth(editPanel.current))
+  })
 
   return (
-    <div className="flex flex-col p-6">
+    <div ref={editPanel} className="flex flex-col p-6">
+      <button
+        onClick={() => setModelState(true)}
+        className="flex h-12 w-full items-center justify-center gap-4 rounded border border-gray font-semibold"
+      >
+        <img src={iconPlus} alt="plus" />
+        創建簽名
+      </button>
       <div className="mb-auto">
-        <button
-          onClick={() => setModelState(true)}
-          className="flex h-12 w-full items-center justify-center gap-4 rounded border border-gray font-semibold"
-        >
-          <img src={iconPlus} alt="plus" />
-          創建簽名
-        </button>
+        {signStore.localList.map(signUrl => (
+          <ImageUrlPreview
+            url={signUrl}
+            width={imageUrlPreviewWidth}
+            height={150}
+            key={signUrl}
+          />
+        ))}
       </div>
 
       <footer>
