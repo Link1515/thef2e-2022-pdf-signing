8fda543a38d0edf25d8d57e78b4472dedfd63697
Link <h26281660@gmail.com>
2022-11-20 21:08:07 +0800

    add react router basename

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -4,23 +4,26 @@ import Home from '../pages/Home'
 import Edit from '../pages/Edit'
 import Final from '../pages/Final'
 
-export const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        index: true,
-        element: <Home />
-      },
-      {
-        path: 'edit',
-        element: <Edit />
-      },
-      {
-        path: 'final',
-        element: <Final />
-      }
-    ]
-  }
-])
+export const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <App />,
+      children: [
+        {
+          index: true,
+          element: <Home />
+        },
+        {
+          path: 'edit',
+          element: <Edit />
+        },
+        {
+          path: 'final',
+          element: <Final />
+        }
+      ]
+    }
+  ],
+  { basename: '/thef2e-2022-pdf-signing' }
+)
